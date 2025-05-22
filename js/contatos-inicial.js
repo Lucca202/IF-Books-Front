55fcf0f9c3823c0a17e7eb0821e0614c5bc14929
Lucca202 <bv3044297@ad.sbv.ifsp.edu.br>
2025-05-21 22:28:33 -0300

    Adição do js de contatos

---
+++ js/contatos-inicial.js
@@ -0,0 +1,40 @@
+/*
+    @author : Lcnato  bv3044297
+    implementacao: 21 / 05/ 2025
+    Ajuste: Comando iniciais
+        add orientaçca a objetos inicial
+*/
+
+var IFbooks = IFbooks || {};  // cria objeto
+
+/**
+ * IFBooks.Teste padrão de classe IFBooks.NomeClasse = (function(){
+ * }());
+ * 
+ */
+IFbooks.Teste = (function () {
+    
+    function Teste() {  // construtor
+        console.log("Executei Construtor");
+    }
+
+    Teste.prototype.iniciar = function(){
+        console.log("Executei Iniciar...");
+    }
+
+    
+    return Teste;
+}());
+
+
+$(function() {
+    var ifbooks = new IFbooks.Teste();
+    ifbooks.iniciar();
+}
+);
+
+// function teste() {
+//     alert("Olá caneco");
+// }
+
+
